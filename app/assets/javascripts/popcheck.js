98709a45ce572acd0a862dca3a420681a5e543ec
jpark1120 <parkj1120@gmail.com>
2014-07-13 17:41:13 -0400

    beginning to add google map api to layout

--- app/assets/javascripts/popcheck.js
+++ app/assets/javascripts/popcheck.js
@@ -12,7 +12,7 @@ function projectData(dataset){
     // output range
     .range([0, height-10]);
 
-  var svg = d3.select("body")
+  var svg = d3.select("#temp")
                .append("svg")
                .attr("width", width)
                .attr("height", height);
@@ -48,7 +48,30 @@ function projectData(dataset){
 // .ease("elastic", 1, 0.15)
 // .ease("elastic", a, p) where a is the amplitude (default 1) p is elasticity (bounciness) measure (default 0.45).
 
-// var data = [400, 260];
+function mapInitialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(40.7160085,-73.9830292),
+    zoom: 16,
+    zoomControl: false,
+    scrollwheel: false,
+    streetViewControl: false
+  };
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+
+  var placeLatLng = new google.maps.LatLng(40.71935601572622, -73.98493988647952);
+
+  // places marker(symbol) on map
+  var placeMarker = new google.maps.Marker({
+      position: placeLatLng,
+      map: map,
+      // icon: {
+      //   path: google.maps.SymbolPath.CIRCLE,
+      //   scale: 10
+      // }
+  });
+}
 
 var data = [ 
   { name: "visitors", checkins: "1517", color: "#78DBE2" },
