c10d5287991cd416feba4d9d44381106193a4656
jpark1120 <parkj1120@gmail.com>
2014-07-28 15:20:01 -0400

    renamed app and updated database.yml to reflect changes in local db

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_les_pop_check_session'
+Rails.application.config.session_store :cookie_store, key: '_curious_drinker_session'
