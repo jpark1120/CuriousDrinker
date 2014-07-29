5bb772558b13d4b29a563bfe34d9e15a6a6b6f36
jpark1120 <parkj1120@gmail.com>
2014-07-29 15:59:17 -0400

    adjusted seed task to incorporate males and females for each venue

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,12 +1,22 @@
+require 'date'
+
 class Place < ActiveRecord::Base
 
   def self.explore
-    url = "https://api.foursquare.com/v2/venues/explore?ll=40.7160085,-73.9830292&client_id=#{Rails.application.secrets.four_api_id}&client_secret=#{Rails.application.secrets.four_api_secret}&v=20140712&query=drinks"
+    url = "https://api.foursquare.com/v2/venues/explore?ll=40.7160085,-73.9830292&client_id=#{Rails.application.secrets.four_api_id}&client_secret=#{Rails.application.secrets.four_api_secret}&v=#{Time.now.strftime("%Y%m%d")}&query=drinks&limit=50"
 
     jsonresponse = JSON.parse(HTTParty.get(url).body)
 
-    venues = jsonresponse["response"]["groups"][0]["items"]
+    return jsonresponse["response"]["groups"][0]["items"]
   end
 
+
+  def self.venue_crowd(venue)
+    url = "https://api.foursquare.com/v2/venues/#{venue}/tips?sort=recent&client_id=#{Rails.application.secrets.four_api_id}&client_secret=#{Rails.application.secrets.four_api_secret}&v=#{Time.now.strftime("%Y%m%d")}&limit=500"
+
+    jsonresponse = JSON.parse(HTTParty.get(url).body)
+
+    return jsonresponse["response"]["tips"]["items"]
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,11 @@ Place.delete_all
 suggested_places = Place.explore
 
 suggested_places.each do |bar|
+  genders = []
+  tips = Place.venue_crowd(bar["venue"]["id"])
+  tips.each do |commenters|
+    genders << commenters["user"]["gender"]
+  end
 
   Place.create({
     name: bar["venue"]["name"],
@@ -10,7 +15,9 @@ suggested_places.each do |bar|
     fsq_visitors: bar["venue"]["stats"]["usersCount"],
     fsq_checkins: bar["venue"]["stats"]["checkinsCount"],
     latitude: bar["venue"]["location"]["lat"].to_s,
-    longitude: bar["venue"]["location"]["lng"].to_s
+    longitude: bar["venue"]["location"]["lng"].to_s,
+    males: genders.count("male"),
+    females: genders.count("female")
   })
 
 end
